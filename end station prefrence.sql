5e6b36d1fb88f2922f85d761438691f27b5ca97e
HindMadkhali <hend.madkhali@gmail.com>
2024-11-21 15:54:42 +0300

    Add files via upload

---
+++ end station prefrence.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select 
+sum(trip_duration)
+from`202004-divvy-tripdata`
+where member_casual = 'member'
+-- Date: 2024-11-21 15:52
+*/
+INSERT INTO `Sum` (`sum(trip_duration)`) VALUES (78282355);
